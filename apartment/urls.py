25b24035be5126195f4d0290849ec3ac9f46f973
pramudya.wibisono <pramudyaw.serious@gmail.com>
2023-01-18 06:45:22 +0700

    Initial apps

---
+++ apartment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+app_name = 'apartment'
+urlpatterns = [
+    # path('', accommodations, name='accommodation_list')
+]
--- apatoman/urls.py
+++ apatoman/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import home.urls as home
+import apartment.urls as apartment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include(home), name='home'),
+    # path('/apartment', include(apartment)),
 ]
